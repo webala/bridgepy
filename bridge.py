f335f9b37cc4c7588719fcef24071ace79cd7998
webala <webdspam@gmail.com>
2023-08-21 17:50:00 +0300

    created a generate payload function

--- bridge.py
+++ bridge.py
@@ -1,6 +1,7 @@
-import requests, os
+import requests, os, base64
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
+from datetime import datetime
 
 #load environment variables
 load_dotenv()
@@ -11,11 +12,13 @@ class Bridge():
     def auth_url(_):
         return "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
     
-    def __init__(self, consumer_key, consumer_secret, business_shortcode, passkey) -> None:
+    def __init__(self, consumer_key, consumer_secret, business_shortcode, passkey, app_name, callback_url) -> None:
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
         self.business_shortcode = business_shortcode
         self.passkey = passkey
+        self.app_name = app_name
+        self.callback_url = callback_url
    
     def authenticate(self):
         '''
@@ -36,16 +39,40 @@ class Bridge():
          print('Authenticate error: ', e)
 
 
-    def _generate_password(self):
-      #  data_to_encode = str(self.business_shortcode + self.passkey + timestamp)
-      pass
-        
+    def _generate_payload(self, phone_number, transaction_description, amount=1):
+       '''
+       This function generates the payload that will be used in the initiate stk push request.
+       '''
+       #Genetate a timeastamp in the format year, month, day, hour, minutes, seconds
+       current_time = datetime.now()
+       timestamp = current_time.strftime('%Y%m%d%H%M%S')
+
+       #Generate the password.
+       #A base64 encoded string. (The base64 string is a combination of Shortcode+Passkey+Timestamp)
+       data_to_encode = self.business_shortcode + self.passkey + timestamp
+       password = base64.b64encode(data_to_encode.encode("utf-8")).decode('utf-8')
+
+       return {    
+            "BusinessShortCode": self.business_shortcode,    
+            "Password": password,    
+            "Timestamp": timestamp,    
+            "TransactionType": "CustomerPayBillOnline",    
+            "Amount": amount,    
+            "PartyA": phone_number,    
+            "PartyB": self.business_shortcode,    
+            "PhoneNumber": phone_number,    
+            "CallBackURL": self.callback_url,
+            "AccountReference": self.app_name,    
+            "TransactionDesc": transaction_description
+        }      
 
 bridge = Bridge(
    consumer_key=os.getenv('CONSUMER_KEY'),
    consumer_secret=os.getenv('CONSUMER_SECRET'),
    business_shortcode=os.getenv('SHORT_CODE'),
-   passkey=os.getenv('PASSKEY')
+   passkey=os.getenv('PASSKEY'),
+   app_name='bridge',
+   callback_url='https://callback.url.com'
 )
 
-print(bridge.authenticate())
+print(bridge._generate_payload(phone_number='254791055897', transaction_description='Bridgepy test'))
