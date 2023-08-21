ed548ae3af1715ac861e011d6d1bd8f6bf8876b1
webala <webdspam@gmail.com>
2023-08-21 18:28:00 +0300

    created initialize stk push function

--- bridge.py
+++ bridge.py
@@ -1,78 +1,118 @@
-import requests, os, base64
+import requests, os, base64, json
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 from datetime import datetime
 
-#load environment variables
+# load environment variables
 load_dotenv()
 
-class Bridge():
 
+class Bridge:
     @property
     def auth_url(_):
         return "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-    
-    def __init__(self, consumer_key, consumer_secret, business_shortcode, passkey, app_name, callback_url) -> None:
+
+    @property
+    def stk_url(_):
+        return "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+    def __init__(
+        self,
+        consumer_key,
+        consumer_secret,
+        business_shortcode,
+        passkey,
+        app_name,
+        callback_url,
+    ) -> None:
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
         self.business_shortcode = business_shortcode
         self.passkey = passkey
         self.app_name = app_name
         self.callback_url = callback_url
-   
+
     def authenticate(self):
-        '''
+        """
         Makes a request to the auth endndpoint and raise an exception if an error response is returned
         On success, status 200, get the access token and return it
-        '''
+        """
         try:
-        
-         response = requests.get(self.auth_url, auth=HTTPBasicAuth(self.consumer_key, self.consumer_secret))
-         response.raise_for_status()
+            response = requests.get(
+                self.auth_url,
+                auth=HTTPBasicAuth(self.consumer_key, self.consumer_secret),
+            )
+            response.raise_for_status()
 
-         json_res = response.json()
-         access_token = json_res.get('access_token')
+            json_res = response.json()
+            access_token = json_res.get("access_token")
 
-         return access_token
-        
+            return access_token
+
         except requests.exceptions.RequestException as e:
-         print('Authenticate error: ', e)
-
+            print("Authenticate error: ", e)
 
     def _generate_payload(self, phone_number, transaction_description, amount=1):
-       '''
-       This function generates the payload that will be used in the initiate stk push request.
-       '''
-       #Genetate a timeastamp in the format year, month, day, hour, minutes, seconds
-       current_time = datetime.now()
-       timestamp = current_time.strftime('%Y%m%d%H%M%S')
+        """
+        This function generates the payload that will be used in the initiate stk push request.
+        """
+        # Genetate a timeastamp in the format year, month, day, hour, minutes, seconds
+        current_time = datetime.now()
+        timestamp = current_time.strftime("%Y%m%d%H%M%S")
 
-       #Generate the password.
-       #A base64 encoded string. (The base64 string is a combination of Shortcode+Passkey+Timestamp)
-       data_to_encode = self.business_shortcode + self.passkey + timestamp
-       password = base64.b64encode(data_to_encode.encode("utf-8")).decode('utf-8')
+        # Generate the password.
+        # A base64 encoded string. (The base64 string is a combination of Shortcode+Passkey+Timestamp)
+        data_to_encode = self.business_shortcode + self.passkey + timestamp
+        password = base64.b64encode(data_to_encode.encode("utf-8")).decode("utf-8")
 
-       return {    
-            "BusinessShortCode": self.business_shortcode,    
-            "Password": password,    
-            "Timestamp": timestamp,    
-            "TransactionType": "CustomerPayBillOnline",    
-            "Amount": amount,    
-            "PartyA": phone_number,    
-            "PartyB": self.business_shortcode,    
-            "PhoneNumber": phone_number,    
+        return {
+            "BusinessShortCode": self.business_shortcode,
+            "Password": password,
+            "Timestamp": timestamp,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone_number,
+            "PartyB": self.business_shortcode,
+            "PhoneNumber": phone_number,
             "CallBackURL": self.callback_url,
-            "AccountReference": self.app_name,    
-            "TransactionDesc": transaction_description
-        }      
+            "AccountReference": self.app_name,
+            "TransactionDesc": transaction_description,
+        }
+
+    def initialize_stk(self, phone_number, transaction_description):
+        access_token = self.authenticate()
+        headers = {"Authorization": f"Bearer {access_token}"}
+        payload = self._generate_payload(phone_number, transaction_description)
+
+        try:
+            response = requests.post(self.stk_url, headers=headers, json=payload)
+            json_res = json.loads(response.text)
+
+            return {
+                "merchant_request_id": json_res.get("MerchantRequestID"),
+                "chechout_request_id": json_res.get("CheckoutRequestID"),
+                "response_code": json_res.get("ResponseCode"),
+                "response_description": json_res.get("ResponseDescription"),
+                "customer_meaasge": json_res.get("CustomerMessage"),
+            }
+
+        except requests.exceptions.RequestException as e:
+            if hasattr(e, "response") and e.response is not None:
+                print("Initiate stk push errored out with code", e.response.status_code)
+                print(json.loads(e.response.text))
+            else:
+                print("An error occurred: ", e)
+
 
 bridge = Bridge(
-   consumer_key=os.getenv('CONSUMER_KEY'),
-   consumer_secret=os.getenv('CONSUMER_SECRET'),
-   business_shortcode=os.getenv('SHORT_CODE'),
-   passkey=os.getenv('PASSKEY'),
-   app_name='bridge',
-   callback_url='https://callback.url.com'
+    consumer_key=os.getenv("CONSUMER_KEY"),
+    consumer_secret=os.getenv("CONSUMER_SECRET"),
+    business_shortcode=os.getenv("SHORT_CODE"),
+    passkey=os.getenv("PASSKEY"),
+    app_name="bridge",
+    callback_url="https://callback.url.com",
 )
 
-print(bridge._generate_payload(phone_number='254791055897', transaction_description='Bridgepy test'))
+print(
+    bridge.initialize_stk("254791055897", "Bridge first test")
+)
